2c7105c7516cd559b3e0562df4d69fd5954c1f85
Aleksei Maslakov <lesha.maslakov@gmail.com>
2020-12-30 13:33:17 +0300

    Add policy text

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from starlette.applications import Starlette
 from starlette.responses import Response
 from starlette.routing import Route
 
+from policy import POLICY_TEXT
+
 
 async def index(request):
-    return Response("Hello, World", media_type="text/plain")
+    return Response(POLICY_TEXT, media_type="text/plain")
 
 app = Starlette(debug=False, routes=[
     Route("/", index),
